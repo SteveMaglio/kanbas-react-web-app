14d370bff8178770b5b5e850267342e2e04b97fd
SteveMaglio <magliocchino.s@northeastern.edu>
2023-10-16 21:45:21 -0400

    working on dashboard

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -4,11 +4,25 @@ function Dashboard() {
   const courses = db.courses;
   return (
     <div>
-      <h1>Dashboard</h1>
-      <div className="list-group">
+      <h2 style={{marginLeft : "36px", marginTop: "18px"}}>Dashboard</h2>
+      <hr></hr>
+      <div className="wd-dashboard list-group d-flex flex-row flex-sm-wrap flex-md-wrap flex-lg-wrap flex-xl-wrap flex-wrap p-2">
         {courses.map((course) => (
-          <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item">
-            {course.name}
+          <Link
+            key={course._id}
+            to={`/Kanbas/Courses/${course._id}`}
+            className="list-group-item"
+          >
+            <img style={{width : "100%"}} src={course.img}></img>
+            <br></br>
+
+            {course.number} <br></br>
+            <h5>{course.name}</h5>
+            <div style={{ fontSize: ".8em" }}>
+              {course._id} <br></br>
+              {course.startDate} <br></br>
+              {course.endDate} <br></br>
+            </div>
           </Link>
         ))}
       </div>
--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -1,14 +1,34 @@
+import "../../Kanbas/style.css";
 import { Link, useLocation } from "react-router-dom";
+import {
+  FaRegUserCircle,
+  FaTachometerAlt,
+  FaBook,
+  FaCalendarAlt,
+} from "react-icons/fa";
 function KanbasNavigation() {
   const links = ["Account", "Dashboard", "Courses", "Calendar"];
+  const icons = [
+    <FaRegUserCircle className="custom-icon" />,
+    <FaTachometerAlt className="custom-icon" />,
+    <FaBook className="custom-icon" />,
+    <FaCalendarAlt className="custom-icon" />,
+  ];
   const { pathname } = useLocation();
   return (
-    <div className="list-group" style={{ width: 150 }}>
+    <div className="wd-kn list-group-flush">
+      <img style={{width : "100%"}} src={"../../images/nulogo.png"}></img>
       {links.map((link, index) => (
         <Link
+          style={{
+            textAlign: "center",
+          }}
           key={index}
           to={`/Kanbas/${link}`}
-          className={`list-group-item ${pathname.includes(link) && "active"}`}>
+          className={`list-group-item ${pathname.includes(link) && "active"}`}
+        >
+          {icons[index]}
+          <br></br>
           {link}
         </Link>
       ))}
